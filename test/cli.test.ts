01149e9b8516b2156bbc2a9d78d64a853727ca94
Suyash Shetty <suyashshetty29@gmail.com>
2026-02-17 08:41:48 +0000

    feat: adopt runCli from core, shared workflows, delete version-bump and versioncmd

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import { parseArgs } from "../src/cli";
+import { parseArgs } from "@shetty4l/core/cli";
 
 describe("parseArgs", () => {
   test("extracts command and args", () => {
