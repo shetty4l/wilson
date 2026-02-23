d20c28021033ae6280bf572e435f4c4307e2d1f6
Suyash Shetty <suyashshetty29@gmail.com>
2026-02-23 18:58:40 +0000

    feat: add includeCalendars config + 30s spawn timeout
    - Add includeCalendars?: string[] to CalendarChannelConfig
    - Add 30-second timeout to defaultSpawn() using Promise.race
    - On timeout: kill proc, return exitCode=-1, stderr='osascript timed out'
    - Log timeout occurrence for observability

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ export interface CalendarChannelConfig {
   pollIntervalSeconds: number;
   lookAheadDays: number;
   extendedLookAheadDays: number;
+  includeCalendars?: string[];
 }
 
 export interface CortexConfig {
